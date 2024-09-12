5d7bf089a8ebcabaa587cf2fe16dbd8c6cd4d544
johan sebastian <baloncestonba222@gmail.com>
2024-09-12 10:30:16 +0200

    - Added appointments section in the dashboard. - Created calendar to manage appointments. - Implemented functionality to switch between sections (appointments, income, settings).

---
+++ app/main/static/js/calendar.js
@@ -0,0 +1,96 @@
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let currentDate = new Date();
+
+export function Calendar() {
+  const daysContainer = document.getElementById('days');
+  const monthYear = document.getElementById('monthYear');
+  daysContainer.innerHTML = '';
+  const appoinmentDate = document.getElementById('appoinmentDate')
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+  const firstDay = new Date(year, month, 1).getDay();
+  const lastDate = new Date(year, month + 1, 0).getDate();
+  const prevLastDate = new Date(year, month, 0).getDate();
+
+  monthYear.textContent = `${monthNames[month]} ${year}`;
+  appoinmentDate.textContent = `Appoiments for ${day}/${month + 1}/${year}`
+
+  renderWeekdays(daysContainer);
+  renderDays(daysContainer, firstDay, prevLastDate, lastDate, year, month);
+}
+
+function renderWeekdays(container) {
+  dayNames.forEach(day => {
+    const dayElement = document.createElement('div');
+    dayElement.classList.add('weekday');
+    dayElement.textContent = day;
+    container.appendChild(dayElement);
+  });
+}
+  
+function renderDays(container, firstDay, prevLastDate, lastDate, year, month) {
+  for (let i = firstDay; i > 0; i--) {
+    createDayElement(container, prevLastDate - i + 1, 'inactive');
+  }
+
+  for (let day = 1; day <= lastDate; day++) {
+    const dayElement = createDayElement(container, day);
+    if (day === currentDate.getDate() && month === new Date().getMonth() && year === new Date().getFullYear()) {
+      dayElement.classList.add('today');
+    }
+    dayElement.onclick = () => handleDayClick(year, month, day);
+  }
+
+  const totalCells = firstDay + lastDate;
+  const nextDays = 42 - totalCells;
+  for (let i = 1; i <= nextDays; i++) {
+    createDayElement(container, i, 'inactive');
+  }
+}
+  
+function createDayElement(container, day, additionalClass = '') {
+  const dayElement = document.createElement('div');
+  dayElement.classList.add('day');
+  if (additionalClass) dayElement.classList.add(additionalClass);
+  dayElement.textContent = day;
+  container.appendChild(dayElement);
+  return dayElement;
+}
+
+export function prevMonth() {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  Calendar();
+}
+  
+export function nextMonth() {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  Calendar();
+}
+
+
+
+
+// function handleDayClick(year, month, day) {
+//     const utcDate = new Date(Date.UTC(year, month, day));
+//     fetch('/view_dates', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ date: utcDate.toISOString() })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       if (data.success) {
+//         updateAppointments(data.appointments,year,month,day);
+//       } else {
+//         alert('Error al actualizar las citas: ' + data.error);
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//       alert('Error al actualizar las citas');
+//     });
+// }
